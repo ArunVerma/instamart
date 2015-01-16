4f4ddff83e5877379706352df67dab3770971c2e
Marc Lang <tradecraft9@gmail.com>
2015-01-16 17:01:20 -0500

    etc

--- db/seed_data/scraper.py
+++ db/seed_data/scraper.py
@@ -13,22 +13,3 @@ sesh = requests.Session()
 target_url = "https://www.instacart.com/store/whole-foods/departments/82/aisles/513"
 bsoup = BeautifulSoup(sesh.get(target_url).content)
 matches = bsoup.find_all(class_='item')
-
-for match in matches:
-    if count == 0:
-        break
-    username = str(match.string)
-    if len(dat.stage[dat.username == username]) != 0:
-        print '[ ] ' + username
-        continue
-    else:
-        msg_data = {'ajax':1, 'sendmsg':1, 'r1':username, 'body':body, 'authcode':auth}
-        send_msg = sesh.get('http://www.okcupid.com/mailbox', params=msg_data)
-        if json.loads(send_msg.content)['status'] == 3:
-            print '[X] ' + username
-            count -= 1
-            dat = dat.append({'username':username, 'stage':1, 'fm_me':cur_time, 'to_me':'', 'location':'NY'}, ignore_index=True)
-            dat.to_csv('users.csv')
-        else:
-            print '[!] ' + username
-            continue
